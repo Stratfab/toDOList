f99884733afe77a41cdd4177e1fb2b217a12e103
Stratfab <fabsiegele@gmail.com>
2021-01-14 22:32:49 +0100

    ajout de commentaires

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
-//  selection de ul / récuperation du form de l'input texte et bouton
+// selection de ul / récuperation du form de l'input texte et bouton
 // eventlistener sur bouton / "(créer elemnts DIV (task)") 
 // assigner contenu DIV depuis text input / ajout bouton supprimer
 // ajout dans ul
 
-
+// Variables pour retourner les objets html à partir de leur id
 var task = document.getElementById("taskList"); 
 var form = document.getElementById("form"); 
 var add = document.getElementById("addTask"); 
 var btn = document.getElementById("submit");
 
 
+// Méthode gestionnnaire d'évènement pour soumettre le formulaire et afficher le résultat
 form.addEventListener('submit', (e) => {
     e.preventDefault(); // Prevents the browser to redirect to another page
-    if(add.value==""){
+    if (add.value=="") {
         add.style.borderColor = 'red';
         return;
-    }else{
+    } else {
         add.style.borderColor = 'silver';
-       var value = add.value;
+        var value = add.value;
         add.value = "";
-
     }
 
-var div = document.createElement("div");
+// Création de l'élément div 
+let div = document.createElement("div");
+// Création de l'élément liste
+let li = document.createElement("li");
 
-
-var li = document.createElement("li");
+// Récupération des tâches dans une li
 li.textContent = value;
 li.classList.add("task");
+
+// Ajout de l'élément li dans div 
 div.appendChild(li);
+// Ajout de l'élément div dans task
 task.appendChild(div); 
 
 
-
-li.addEventListener('click', function(){
+// Méthode pour barrer les tâches
+li.addEventListener('click', function() {
    li.style.textDecoration = "line-through";
 
 });
 
-li.addEventListener('dblclick', function(){
+
+// Méthode pour supprimer les tâches 
+li.addEventListener('dblclick', function() {
   task.removeChild(div);
 
-
 });
-
 
 
 });
